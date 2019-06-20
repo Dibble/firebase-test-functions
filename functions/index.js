b1c60f514c114cf6bb3fdde37475033e0952ad2f
Dibble <james.cotterell@newvoicemedia.com>
2019-06-20 14:43:24 +0100

    add user to firestore on creation

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!")
+})
+
+exports.createUser = functions.auth.user().onCreate(async (user) => {
+  let writeResult = await admin.firestore().collection('users').add({ userUID: user.uid })
+  console.log(writeResult)
+})
